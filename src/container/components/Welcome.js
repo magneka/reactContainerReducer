7fa7c2c852423970e352ca5921af9209af29824f
Magne Alvheim <magne@alvheim.com>
2020-11-21 14:02:59 +0100

    tt

--- src/container/components/Welcome.js
+++ src/container/components/Welcome.js
@@ -1,8 +1,19 @@
-export const Welcome = (props) => {
+import React from "react";
+import urlrikenlogo from "../../assets/logosm.png";
+
+const Welcome = (props) => {
   return (
     <>
-      <h1>Velkommen URL-Riken.</h1>
-      <p>Faggruppe med fokus på frontend</p>
+      <p>Klikk på logo for å fortsette</p>
+
+      <img
+        onClick={props.next}
+        src={urlrikenlogo}
+        style={{ height: "500px" }}
+        alt="URL-RIKEN merc storee"
+      />
     </>
   );
 };
+
+export default Welcome;
